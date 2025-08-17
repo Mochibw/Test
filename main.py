47440e67633680c0295814eaa75ba344ec07e9f1
hubowu <hubowu233@gmail.com>
2025-08-17 18:19:17 +0800

    修复manba没有man的错误

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 def manbaout():
+    return "man!"
 
 def man():
+    return 0
 
 def main():
     print("Hello from primerpioneer!")
 
 def conflict():
+    return 0
 
 
 if __name__ == "__main__":
