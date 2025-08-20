5ae886e05374f7e381c8fb8ce530e1c933070302
hubowu <hubowu233@gmail.com>
2025-08-20 15:03:07 +0800

    Revert "修复manba没有man的错误"
    This reverts commit 47440e67633680c0295814eaa75ba344ec07e9f1.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 def manbaout():
-    return "man!"
 
 def man():
-    return 0
 
 def main():
     print("Hello from primerpioneer!")
 
 def conflict():
-    return 0
 
 def test2():
     return 2
